6c49266683607d8acfb792ecdebd10e5d5f4ee65
Miguel Rodrigues <61872935+cletorodrigues@users.noreply.github.com>
2023-08-22 13:38:47 +0000

    communicationFunc has aggregation almost working it still doens't stop

--- classdrone.py
+++ classdrone.py
@@ -49,16 +49,77 @@ class CrazyflieDrone:
         # Other initializations...
     
     def init_devices(self):
-        # Code to initialize motors, receiver, emitter, etc.
+        ## Initialize motors
+        m1_motor = self.robot.getDevice("m1_motor");
+        m1_motor.setPosition(float('inf'))
+        m1_motor.setVelocity(-1)
+        m2_motor = self.robot.getDevice("m2_motor");
+        m2_motor.setPosition(float('inf'))
+        m2_motor.setVelocity(1)
+        m3_motor = self.robot.getDevice("m3_motor");
+        m3_motor.setPosition(float('inf'))
+        m3_motor.setVelocity(-1)
+        m4_motor = self.robot.getDevice("m4_motor");
+        m4_motor.setPosition(float('inf'))
+        m4_motor.setVelocity(1)
 
-    def init_sensors(self):
-        # Code to initialize IMU, GPS, gyro, etc.
+        ## Initialize Sensors
+        imu = self.robot.getDevice("inertial_unit")
+        imu.enable(self.timestep)
+        gps = self.robot.getDevice("gps")
+        gps.enable(self.timestep)
+        gyro = self.robot.getDevice("gyro")
+        gyro.enable(self.timestep)
+        range_front = self.robot.getDevice("range_front")
+        range_front.enable(self.timestep)
+        range_left = self.robot.getDevice("range_left")
+        range_left.enable(self.timestep)
+        range_back = self.robot.getDevice("range_back")
+        range_back.enable(self.timestep)
+        range_right = self.robot.getDevice("range_right")
+        range_right.enable(self.timestep)
 
-    def send_message(self, message, is_operat):
-        # The message sending logic here
+        ## Get keyboard
+        keyboard = Keyboard()
+        keyboard.enable(self.timestep)
 
-    def get_positions(self):
-        # Logic to fetch positions...
+        #get device name
+        robot_name = self.robot.getName()
+        #print(robot_name)	
+
+        my_id = int(robot_name)
+
+        # Get emitter
+        emitter = self.robot.getDevice("emitter")
+        
+        #Get receiver
+        receiver = self.robot.getDevice("receiver")
+        receiver.enable(self.timestep)
+
+
+    def communicate(self):
+        def send_message(message, is_operat):
+            message_to_send = f"{self.my_id}:{message}:{is_operat}"
+            self.emitter.send(message_to_send.encode('utf-8'))
+
+        def receive_message():
+            TIMEOUT = 50  # This can be adjusted based on your needs.
+
+            all_op = np.zeros(8)
+            timeout_counter = 0
+            while self.receiver.getQueueLength() < 7:
+                if timeout_counter > TIMEOUT:
+                    print(f"Timeout occurred for Drone {self.my_id} waiting for messages. Proceeding with {self.receiver.getQueueLength()} messages.")
+                    break
+            
+
+            
+        while True:
+            with self.lock:
+                send_message()
+                receive_message()
+            self.robot.step(self.timestep)
+
 
     def aggregate(self, pos, avg_pos, coeff_vec, drone_radius):
         # Logic to compute aggregate...
